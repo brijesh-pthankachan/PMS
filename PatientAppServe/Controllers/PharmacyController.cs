4772ac6ca519c54007a0baa93210dfc1ed77ffc4
namitha.augustine <namitha.augustine@NAMITHAAUGUSTIN>
2022-12-16 15:15:26 +0530

    Added Pharmacy Controller

--- PatientAppServe/Controllers/PharmacyController.cs
+++ PatientAppServe/Controllers/PharmacyController.cs
@@ -1,11 +1,53 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PatientAppServe.Models;
+using PatientsAppServer.Data;
 
 namespace PatientAppServe.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[Action]")]
     [ApiController]
     public class PharmacyController : ControllerBase
     {
+        private readonly ApplicationDbContext _db;
+
+        public PharmacyController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        [HttpGet]
+        public IActionResult GetParkedAppointments(int doctorId)
+        {
+            if (_db.Consultations == null) return Ok();
+            var parkedPatients =
+                _db.Consultations.Where(m => m.Status == "Parked" && m.Date.Date == DateTime.UtcNow.Date).ToList();
+            return Ok(parkedPatients);
+
+        }
+
+        [HttpGet]
+        public IActionResult GetCompletedAppointments(int doctorId)
+        {
+            if (_db.Consultations == null) return Ok();
+            var completedPatients =
+                _db.Consultations.Where(m => m.Status == "Completed" && m.Date.Date == DateTime.UtcNow.Date).ToList();
+            return Ok(completedPatients);
+
+        }
+
+        [HttpPost("{appointmentId:int}")]
+        public async Task<IActionResult> CompletePatient(Consultation model, int appointmentId)
+        {
+            if (_db.Consultations == null) return Ok();
+            var patient = await _db.Consultations.FindAsync(appointmentId);
+            if (patient != null)
+            {
+                
+                patient.Status = "Completed";
+            }
+
+            await _db.SaveChangesAsync();
+            return Ok();
+        }
     }
 }
